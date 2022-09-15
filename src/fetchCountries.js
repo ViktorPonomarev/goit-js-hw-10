5c3fa43a8360e0497d37e9d2cff8f9a50810883a
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-09-15 18:04:12 +0300

    Применить приём Debounce

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,36 @@
-export { fetchCountries };
+/* Вынеси её в отдельный файл fetchCountries.js и сделай именованный экспорт.
+ */
+ export { fetchCountries };
 
+ /* Напиши функцию fetchCountries(name) которая делает HTTP-запрос на
+ресурс name и возвращает промис с массивом стран - результатом запроса. 
+ */
 function fetchCountries(name) {
+    
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
+    
+      if (!response.ok) {
+        
+          throw new Error(response.status);
+      }
+      
+      return response.json();
+       
   });
+   
 }
+
+// console.log(fetchCountries(name));  
+
+// fetch('https://restcountries.com/v3.1/name/peru')
+//     .then(response => {
+//         return response.json();
+//     })
+//     .then(name => {
+//         console.log(name);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
+
+/*Вынеси её в отдельный файл fetchCountries.js и сделай именованный экспорт.
+ */
 import { fetchCountries } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+// Достаём елементы
+const refs = {
+    inputForm: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+};
 
-const r = fetch('https://restcountries.com/v3.1/name/peru');
-console.log(r);
+const DEBOUNCE_DELAY = 300;
+// Применить приём Debounce
+refs.inputForm.addEventListener('input',
+    debounce(onInputChenge, DEBOUNCE_DELAY));
