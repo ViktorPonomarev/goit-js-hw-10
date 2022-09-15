078247995bf4aca887d8d834f3bf6d27f1fd7d33
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-09-15 14:34:31 +0300

    Добавил первую часть

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const r = fetch('https://restcountries.com/v3.1/name/peru');
+console.log(r);
